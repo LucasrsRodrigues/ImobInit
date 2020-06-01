3cba630c5fe7e5a9a347ea946a21e0f9b4b6f09e
LucasrsRodrigues <lucasrs.rodrigues3@outlook.com>
2020-06-01 16:07:51 -0300

    Configuracao do Laravel Mix

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Imobinit\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use Imobinit\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('admin.index');
+    }
+
+}
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <link rel="stylesheet" href="{{ url(mix('backend/assets/css/reset.css')) }}"/>
+    <link rel="stylesheet" href="{{ url(mix('backend/assets/css/boot.css')) }}"/>
+    <link rel="stylesheet" href="{{ url(mix('backend/assets/css/login.css')) }}"/>
+    <link rel="icon" type="image/png" href="backend/assets/images/favicon.png"/>
+
+    <title>UpAdmin - Site Control</title>
+</head>
+<body>
+
+<div class="ajax_response"></div>
+
+<div class="dash_login">
+    <div class="dash_login_left">
+        <article class="dash_login_left_box">
+            <header class="dash_login_box_headline">
+                <div class="dash_login_box_headline_logo icon-imob icon-notext"></div>
+                <h1>Login</h1>
+            </header>
+
+            <form name="login" action="" method="post" autocomplete="off">
+                <label>
+                    <span class="field icon-envelope">E-mail:</span>
+                    <input type="email" name="email" placeholder="Informe seu e-mail" required/>
+                </label>
+
+                <label>
+                    <span class="field icon-unlock-alt">Senha:</span>
+                    <input type="password" name="password_check" placeholder="Informe sua senha" required/>
+                </label>
+
+                <button class="gradient gradient-orange radius icon-sign-in">Entrar</button>
+            </form>
+
+            <footer>
+                <p>Desenvolvido por <a href="https://github.com/LucasrsRodrigues/">Lucas Rodrigues</a></p>
+
+            </footer>
+        </article>
+    </div>
+
+    <div class="dash_login_right"></div>
+
+</div>
+
+<script src="{{ url(mix('backend/assets/js/jquery.js')) }}"></script>
+
+</body>
+</html>
